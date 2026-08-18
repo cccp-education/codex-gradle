9d3cfd620766428e0b1e930fef8afa5a1f15f92c
cheroliv <cheroliv@users.noreply.github.com>
2026-08-18 18:34:59 +0200

    chore(pub): bump BOM 0.0.19 → 0.0.20 (S206)

--- codex-plugin/build.gradle.kts
+++ codex-plugin/build.gradle.kts
@@ -25,7 +25,7 @@ version = libs.versions.codex.plugin.get()
 
 dependencies {
     // Import BOM
-    implementation(platform("education.cccp:workspace-bom:0.0.19"))
+    implementation(platform("education.cccp:workspace-bom:0.0.20"))
 
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.serialization.json)
